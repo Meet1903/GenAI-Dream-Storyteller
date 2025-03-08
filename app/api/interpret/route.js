180c82d2a31b7a939dc38e6fd6f7256c7cdd79ed
Meet1903 <meet.diwan.md@gmail.com>
2025-03-08 03:08:03 -0500

    Refactor POST request handler: remove unused variables and streamline prompt generation

--- app/api/interpret/route.js
+++ app/api/interpret/route.js
@@ -6,7 +6,7 @@ const genAI = new GoogleGenerativeAI(process.env.GOOGLE_AI_API_KEY);
 export async function POST(req, res) {
     try {
         
-        const { dream, story, conversationHistory, followUpQuestion } = await req.json();
+        const { dream } = await req.json();
 
         if (!dream || dream.trim() === '') {
             return NextResponse.json({ error: "Invalid dream" }, { status: 400 });
@@ -18,21 +18,6 @@ export async function POST(req, res) {
                 so that I can revisit the dream, in human like language with no extra knowledge bombardment. 
                 Don't use heavy words, keep it simple and easy to understand.
                 Don't use "here's your story" or something similar, just give me the story in human language: ${dream}`;
-        
-        if (conversationHistory && conversationHistory.length > 0) {
-            prompt = `I had a dream, ${dream}, You have created a story based on my dream, 
-            which is ${story}, below are the questions and answers we had during the conversation:`;
-
-            conversationHistory.forEach((item) => {
-                prompt += `\nQuestion: ${item.question}\nAnswer: ${item.answer}`;
-            });
-
-            prompt += `\nNow, I have one more question for you, you can take creative liberty to answer it. 
-            Answer this question: ${followUpQuestion} `;
-        } else if (followUpQuestion) {
-            prompt = `I had a dream, ${dream}, and you created this wonderful story ${story}. Now I have one more question for you, you can take creative liberty to answer it. 
-            Answer this question: ${followUpQuestion} `;
-        }
 
         const result = await model.generateContent({
             contents: [{ role: "user", parts: [{ text: prompt }] }], 
